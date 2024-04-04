bf1d57ac247553d5518d350ab711d477432332dd
Olha Mantuliak <dev@whcrow.od.ua>
2024-04-04 18:00:35 +0300

    Simplified CachingViewProvider component

--- src/main/java/ua/od/whcrow/samples/spring6/boot_web/_global/web/CachingViewProvider.java
+++ src/main/java/ua/od/whcrow/samples/spring6/boot_web/_global/web/CachingViewProvider.java
@@ -9,14 +9,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.View;
-import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.spring6.view.ThymeleafView;
 import ua.od.whcrow.samples.spring6.boot_web._commons.util.Assert;
 import ua.od.whcrow.samples.spring6.boot_web._commons.util.Msg;
 import ua.od.whcrow.samples.spring6.boot_web._commons.web.ViewProvider;
 
-import java.util.Collection;
 import java.util.Locale;
 import java.util.Objects;
 
@@ -27,19 +26,20 @@ class CachingViewProvider implements ViewProvider {
 	
 	@Nullable
 	private final Locale defaultLocale;
-	private final Collection<ViewResolver> viewResolvers;
+	private final ContentNegotiatingViewResolver viewResolver;
 	private final SpringTemplateEngine thymeleafEngine;
 	
 	public CachingViewProvider(@Value(WebConstants.AP_VP_DEFAULT_LOCALE) String defaultLocaleStr,
-			Collection<ViewResolver> viewResolvers, SpringTemplateEngine thymeleafEngine) {
+			ContentNegotiatingViewResolver viewResolver, SpringTemplateEngine thymeleafEngine) {
 		this.defaultLocale = StringUtils.isEmpty(defaultLocaleStr) ? null : LocaleUtils.toLocale(defaultLocaleStr);
-		this.viewResolvers = viewResolvers;
+		this.viewResolver = viewResolver;
 		this.thymeleafEngine = thymeleafEngine;
 	}
 	
 	private boolean isThymeleafViewResolvable(@Nonnull ThymeleafView view) {
 		return thymeleafEngine.getConfiguration().getTemplateResolvers().stream()
-				.map(tr -> tr.resolveTemplate(thymeleafEngine.getConfiguration(), null, view.getTemplateName(), null))
+				.map(resolver -> resolver.resolveTemplate(thymeleafEngine.getConfiguration(), null,
+						view.getTemplateName(), null))
 				.anyMatch(Objects::nonNull);
 	}
 	
@@ -48,24 +48,21 @@ class CachingViewProvider implements ViewProvider {
 	public View getView(@Nonnull String viewName, @Nonnull Locale locale) {
 		Assert.notNull(viewName, "viewName");
 		Assert.notNull(locale, "locale");
-		for (ViewResolver viewResolver : viewResolvers) {
-			try {
-				View view;
-				if ((view = viewResolver.resolveViewName(viewName, locale)) != null) {
-					// ThymeleafViewResolver does not actually check template for availability,
-					// this check is performed only during template processing. So we need to check it ourselves.
-					if (view instanceof ThymeleafView thymeleafView) {
-						if (!isThymeleafViewResolvable(thymeleafView)) {
-							continue;
-						}
-					}
-					return view;
-				}
-			} catch (Exception e) {
-				log.error(Msg.format("Failed to provide a view '{}' for locale {}", viewName, locale), e);
+		try {
+			View view = viewResolver.resolveViewName(viewName, locale);
+			if (view == null) {
+				return null;
 			}
+			// ThymeleafViewResolver does not actually check template for availability,
+			// this check is performed only during template processing. So we need to check it ourselves.
+			if (view instanceof ThymeleafView thymeleafView && !isThymeleafViewResolvable(thymeleafView)) {
+				return null;
+			}
+			return view;
+		} catch (Exception e) {
+			log.error(Msg.format("Failed to provide a view '{}' for locale {}", viewName, locale), e);
+			return null;
 		}
-		return null;
 	}
 	
 	@Nullable
