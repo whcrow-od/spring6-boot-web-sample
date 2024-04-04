8d68b7c146dff00be8f6839576e9a36359e97a3e
Olha Mantuliak <dev@whcrow.od.ua>
2024-04-04 16:24:16 +0300

    Refactoring the Msg utility class to get rid of inappropriate use of Slf4J

--- src/main/java/ua/od/whcrow/samples/spring6/boot_web/_commons/util/Msg.java
+++ src/main/java/ua/od/whcrow/samples/spring6/boot_web/_commons/util/Msg.java
@@ -1,14 +1,17 @@
 package ua.od.whcrow.samples.spring6.boot_web._commons.util;
 
 import jakarta.annotation.Nonnull;
-import org.slf4j.helpers.MessageFormatter;
+import jakarta.annotation.Nullable;
 import ua.od.whcrow.samples.spring6.boot_web._commons.exceptions.UtilClassInstantiationException;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 
 public final class Msg {
+	
+	public static final String DELIMITER = "{}";
 	
 	private Msg()
 			throws UtilClassInstantiationException {
@@ -16,18 +19,7 @@ public final class Msg {
 	}
 	
 	@Nonnull
-	public static String format(@Nonnull String msg, Object... args) {
-		Assert.notNull(msg, "message text");
-		for (int i = 0; i < args.length; i++) {
-			if (args[i] != null && args[i].getClass().isArray()) {
-				args[i] = wrapArrayToList(args[i]);
-			}
-		}
-		return MessageFormatter.arrayFormat(msg, args).getMessage();
-	}
-	
-	@Nonnull
-	private static List<Object> wrapArrayToList(@Nonnull Object arrayObject) {
+	private static List<Object> _convertArrayObjectToList(@Nonnull Object arrayObject) {
 		if (!arrayObject.getClass().getComponentType().isPrimitive()) {
 			return Arrays.asList((Object[]) arrayObject);
 		}
@@ -37,6 +29,55 @@ public final class Msg {
 			array[i] = Array.get(arrayObject, i);
 		}
 		return Arrays.asList(array);
+	}
+	
+	@Nonnull
+	public static String stringify(@Nullable Object o) {
+		if (o == null) {
+			return "NULL";
+		}
+		if (o.getClass().isArray()) {
+			o = _convertArrayObjectToList(o);
+		}
+		return o.toString();
+	}
+	
+	@Nonnull
+	public static String format(@Nonnull Function<Object,String> stringifier, @Nonnull String delimiter,
+			@Nonnull String msg, Object... args) {
+		Assert.notNull(stringifier, "stringifier");
+		Assert.notBlank(delimiter, "delimiter");
+		Assert.notNull(msg, "msg");
+		StringBuilder builder = new StringBuilder(msg.length() + (args.length * 32));
+		for (int argIndex = 0, delimiterIndex, textIndex = 0; argIndex < args.length; argIndex++) {
+			delimiterIndex = msg.indexOf(delimiter, textIndex);
+			if (delimiterIndex == -1) {
+				if (textIndex == 0) {
+					return msg;
+				}
+				return builder.append(msg, textIndex, msg.length()).toString();
+			}
+			builder.append(msg, textIndex, delimiterIndex);
+			String strArg;
+			try {
+				strArg = stringifier.apply(args[argIndex]);
+			} catch (Exception e) {
+				strArg = "!STRINGIFIER-EXCEPTION " + e + "!";
+			}
+			builder.append(strArg);
+			textIndex = delimiterIndex + delimiter.length();
+		}
+		return builder.toString();
+	}
+	
+	@Nonnull
+	public static String format(@Nonnull Function<Object,String> stringifier, @Nonnull String msg, Object... args) {
+		return format(stringifier, DELIMITER, msg, args);
+	}
+	
+	@Nonnull
+	public static String format(@Nonnull String msg, Object... args) {
+		return format(Msg::stringify, msg, args);
 	}
 	
 }
